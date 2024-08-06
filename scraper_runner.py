f330c4aa5d661d476f0748e8c85fde1e7196bee0
MatthewGanaden <70753819+MatthewGanaden@users.noreply.github.com>
2024-08-07 00:59:00 +0800

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -3,6 +3,6 @@ import scraper as fs
 keywords = ["Baguio Fire"] 
 time = 4 
 csv_file = "Sample.csv" 
-username = "123happyjose321@gmail.com"
-password = "HappyJose123"
+username = ""
+password = ""
 fs.search_fb(username, password, keywords, time, csv_file)
